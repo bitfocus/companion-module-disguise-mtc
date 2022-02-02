69d48badb3ef68211a2d36bf5380cbfd83c5bacc
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:24 +0000

    bulk replace system.emit calls

--- disguise-mtc.js
+++ disguise-mtc.js
@@ -136,7 +136,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 		'timecode': {
 			label: 'Goto timecode',
 			options: [
